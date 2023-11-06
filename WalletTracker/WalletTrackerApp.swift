87d53e586bb422830fedbf1ecfa62859e56cd5bc
4050 <stasguest@gmail.com>
2023-11-06 21:49:36 +0200

    Initial Commit

---
+++ WalletTracker/WalletTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletTrackerApp.swift
+//  WalletTracker
+//
+//  Created by Станислав on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WalletTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
